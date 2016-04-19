542600d504fd57c3b998b65f9508033d9db57135
Christian Mello <ecksian@icloud.com>
2016-04-18 23:08:52 -0700

    Added card effects as separate files

---
+++ dominion/cardEffects.h
@@ -0,0 +1,11 @@
+#ifndef _CARD_EFFECTS_H
+#define _CARD_EFFECTS_H
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+
+int baronEffect(struct gameState *state, int choice1);
+int stewardEffect(struct gameState *state, int handPos, int choice1, int choice2, int choice3);
+int ambassadorEffect(struct gameState *state, int handPos, int choice1, int choice2);
+int embargoEffect(struct gameState *state, int handPos, int choice1);
+int minionEffect(struct gameState *state, int handPos, int choice1, int choice2);
