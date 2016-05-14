63c60b7215aa64b626338aead953dc441c6d3114
Christian Mello <ecksian@icloud.com>
2016-05-13 20:13:12 -0700

    Added beautification to files.

--- dominion/cardtest1.c
+++ dominion/cardtest1.c
@@ -6,31 +6,31 @@
 int failed = 0;
 
 void myassert(int b,char* msg) {
-  if (b == 0) {
-    printf("FAILED ASSERTION: %s\n",msg);
-    failed = 1;
-  }
+        if (b == 0) {
+                printf("FAILED ASSERTION: %s\n",msg);
+                failed = 1;
+        }
 }
 
 void checkasserts() {
-  if (!failed) {
-    printf ("SMITHY TEST SUCCESSFULLY COMPLETED.\n");
-  }
+        if (!failed) {
+                printf ("SMITHY TEST SUCCESSFULLY COMPLETED.\n");
+        }
 }
 
 int main() {
-  struct gameState g;
+        struct gameState g;
 
-  int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
-	       outpost,baron,tribute};
+        int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
+                     outpost,baron,tribute};
 
-  initializeGame(2, k, 5, &g);
+        initializeGame(2, k, 5, &g);
 
-  int startingHand = numHandCards(&g);
+        int startingHand = numHandCards(&g);
 
-  myassert(cardEffect(smithy, 0, 0, 0, &g, 0, 0), "Smithy returned the wrong value.");
+        myassert(cardEffect(smithy, 0, 0, 0, &g, 0, 0), "Smithy returned the wrong value.");
 
-  myassert(numHandCards(&g) == startingHand + 3, "Wrong number of cards drawn by smithy.");
+        myassert(numHandCards(&g) == startingHand + 3, "Wrong number of cards drawn by smithy.");
 
-  checkasserts();
+        checkasserts();
 }
