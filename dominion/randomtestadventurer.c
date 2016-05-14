63ab6cd8dec0f74f62eb9eb73116558ef43a970b
Christian Mello <ecksian@icloud.com>
2016-05-13 21:17:41 -0700

    Started work on random test cards. Oye.

---
+++ dominion/randomtestadventurer.c
@@ -0,0 +1,36 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int failed = 0;
+
+void myassert(int b,char* msg) {
+        if (b == 0) {
+                printf("FAILED ASSERTION: %s\n",msg);
+                failed = 1;
+        }
+}
+
+void checkasserts() {
+        if (!failed) {
+                printf ("ADVENTURER TEST SUCCESSFULLY COMPLETED.\n");
+        }
+}
+
+int main() {
+        struct gameState g;
+
+        int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
+                     outpost,baron,tribute};
+
+        initializeGame(2, k, 5, &g);
+
+        int startingHand = numHandCards(&g);
+
+        myassert(cardEffect(adventurer, 0, 0, 0, &g, 0, 0), "Adventurer returned the wrong value.");
+
+        myassert(numHandCards(&g) == startingHand + 3, "Wrong number of cards drawn by adventurer.");
+
+        checkasserts();
+}
