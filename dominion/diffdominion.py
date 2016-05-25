0eaf14405dc5b6fff71c96e0281efd918dfffe2f
Christian Mello <ecksian@icloud.com>
2016-05-24 23:56:49 -0700

    Finished Assignment 4

--- dominion/diffdominion.py
+++ dominion/diffdominion.py
@@ -7,11 +7,15 @@ seed = sys.argv[3]
 originalDirectory = os.getcwd()
 
 os.chdir(g1)
-os.system("make clean; make testdominion; ./testdominion " + seed + " > " + originalDirectory + "/out1")
+os.system("make clean")
+os.system("make testdominion")
+print("./testdominion " + seed + " > " + originalDirectory + "/out1")
+os.system("./testdominion " + seed + " > " + originalDirectory + "/out1")
 
-os.chdir(originalDirectory)
-os.chdir(g2)
-os.system("make clean; make testdominion; ./testdominion " + seed + " > " + originalDirectory + "/out2")
+os.chdir(originalDirectory + "/" + g2)
+os.system("make clean")
+os.system("make testdominion")
+os.system("./testdominion " + seed + " > " + originalDirectory + "/out2")
 
 f1 = originalDirectory + "/out1"
 f2 = originalDirectory + "/out2"
@@ -44,4 +48,6 @@ for i in xrange(0,min(len(l1),len(l2))):
         print "PRIOR TO DIVERGENCE:"
         print f1,(l1[i-1][1])[:-1]
         print f2,(l2[i-1][1])[:-1]
+        print "UNIX DIFF OUTPUT:"
+        # os.system("diff -c " + originalDirectory + "/out1 " + originalDirectory + "/out2")
         break
